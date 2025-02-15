7d603c5793d171eebd9d20ec1c512965c4f0eec6
Tien Vo <tienhoangvo.dev@gmail.com>
2025-02-15 21:52:59 +0000

    fix: ui button

--- src/components/ui/Button/Button.tsx
+++ src/components/ui/Button/Button.tsx
@@ -1,46 +1,33 @@
-import clsx from 'clsx';
-import type { ComponentProps, ElementType, ReactNode } from 'react';
-import buttonStyles from './Button.module.css';
-import Text from '../Text';
+import clsx from "clsx";
+import type { ComponentProps, ElementType, ReactNode } from "react";
+import buttonStyles from "./Button.module.css";
+import Text from "../Text";
 
 type ButtonOwnProps<E extends ElementType> = {
-    as?: E;
-    glow?: boolean;
-    children: ReactNode;
-    className?: string;
+  as?: E;
+  glow?: boolean;
+  children: ReactNode;
+  className?: string;
 };
 
-type ButtonProps<E extends ElementType> = ButtonOwnProps<E> & Omit<ComponentProps<E>, keyof ButtonOwnProps<E>>;
+type ButtonProps<E extends ElementType> = ButtonOwnProps<E> &
+  Omit<ComponentProps<E>, keyof ButtonOwnProps<E>>;
 
-export default function Button<E extends ElementType = 'button'>({
-    as,
-    glow,
-    children,
-    className,
-    ...delegated
+export default function Button<E extends ElementType = "button">({
+  as,
+  glow,
+  children,
+  className,
+  ...delegated
 }: ButtonProps<E>) {
-    const Component = as || 'button';
-    return (
-        <Component
-            {...delegated}
-            className={clsx(buttonStyles.Button, className)}
-        >
-            {glow && (
-                <span
-                    aria-hidden
-                    className={buttonStyles.Glow}
-                />
-            )}
-            <span
-                aria-hidden
-                className={buttonStyles.NeumorphicLayer}
-            />
-            <Text
-                as='span'
-                className={buttonStyles.Label}
-            >
-                {children}
-            </Text>
-        </Component>
-    );
+  const Component = as || "button";
+  return (
+    <Component {...delegated} className={clsx(buttonStyles.Button, className)}>
+      {glow && <span aria-hidden className={buttonStyles.GlowLayer} />}
+      <span aria-hidden className={buttonStyles.NeumorphicLayer} />
+      <Text as="span" className={buttonStyles.Label}>
+        {children}
+      </Text>
+    </Component>
+  );
 }
